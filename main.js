31af7bde4186c4101a5c79a1ecbcac9f61ebdeab
IIIgorMoura <igor2007moura@gmail.com>
2023-11-07 08:52:32 -0300

    a

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 const novaNota = document.getElementById("nota-nota")
 const adicionarNota = document.getElementById("adicionar-nota")
+const limparNotas = document.getElementById("limpar-notas")
+const notasSalvas = document.getElementById("notas-salvas")
+
+// verif se tem alguma nota no arnazenamento local
+if (localStorage.getItem("notas")) {
+    // recupera o valor do item "notas" do localStorage e converte de volta para JS usando JSON.parse()
+    const notas = JSON.parse(localStorage.getItem("notas"))
+
+    // percorre cada nota usando loop e usa funcao criarNota()
+    notas.forEach(function (nota, index) {
+        criarNota(nota, index)
+    })
+}
